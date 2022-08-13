a7c69c559af6370fb8a4feccf73ae36a88ba0a40
gabrieldasilvaa&*@gmail.com <gabrieldasilvaa789@gmail.com>
2022-08-13 02:09:30 -0300

    funcionalidade login

--- entrar.js
+++ entrar.js
@@ -1,15 +1,33 @@
-document.getElementById('btn-entrar')
-.addEventListener('click', entrar);
+document.getElementById("btn-entrar").addEventListener("click", entrar);
 
-document.getElementById('btn-cadastro')
-.addEventListener('click', cadastro);
+document
+  .getElementById("btn-cadastro")
+  .addEventListener("click", realizarCadastro);
 
-let listaDeUsuarios = JSON.parse(localStorage.getItem('usuarios') || '[]');
+let listaDeUsuarios = JSON.parse(localStorage.getItem("usuarios") || "[]");
 
 function entrar() {
-    console.log('entrar')
+  if (!email.value || !senha.value) {
+    alert("Campos Vazios");
+    return;
+  }
+
+  const usuario = listaDeUsuarios.find(
+    (usuario) => usuario.email === email.value && usuario.senha === senha.value
+  );
+
+  if (!usuario) {
+    alert("Usuario invalido ou inexistente");
+    return;
+  }
+
+  abrirListaDeRecados();
 }
 
-function cadastro() {
-    window.location.href = 'cadastro.html';
+function abrirListaDeRecados() {
+  window.location.href = "lista.html";
+}
+
+function realizarCadastro() {
+  window.location.href = "cadastro.html";
 }
