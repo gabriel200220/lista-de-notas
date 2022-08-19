a507a6ef93b1ba30412faf27daa6484df88f1653
gabrieldasilvaa&*@gmail.com <gabrieldasilvaa789@gmail.com>
2022-08-19 01:10:17 -0300

    alteração na lista

--- lista.js
+++ lista.js
@@ -1,3 +1,6 @@
-* {
-    
-}
+let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
+let salvar = document.querySelector('#inputNovaTarefa');
+let ListaTarefas = document.querySelector('#inputNovaTarefa');
+
+  
+
